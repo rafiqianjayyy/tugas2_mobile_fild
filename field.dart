9bd0796ec896e9c7e4f010052d97ba3212ebaad5
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-22 09:54:06 +0700

    Create field.dart

---
+++ field.dart
@@ -0,0 +1,16 @@
+class person {
+    String name = "Rafiqi";
+    String? address;
+    final String country = "banjarmaisn";
+    
+}
+
+void main(){
+  var person1 = person();
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
+    person person2 = person();
+    print(person2);
+}
